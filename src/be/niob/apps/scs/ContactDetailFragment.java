a8dfff24bcb596199c047aa39bd79b2a41b99589
niob <versluyssander@gmail.com>
2012-05-15 17:21:41 +0200

    add (unfinished) loader code for detail fragment

--- src/be/niob/apps/scs/ContactDetailFragment.java
+++ src/be/niob/apps/scs/ContactDetailFragment.java
@@ -4,34 +4,47 @@ import java.io.InputStream;
 
 import android.content.ContentResolver;
 import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class ContactDetailFragment extends Fragment {
+public class ContactDetailFragment extends Fragment 
+	implements LoaderManager.LoaderCallbacks<Cursor> {
  	
 	private static final String TAG = Util.TAG + ":ContactDetailFragment";
 	
 	private ImageView mContactPhoto;
+	private ImageView mGravatar;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View  view = inflater.inflate(R.layout.contact_detail, container);
 		mContactPhoto = (ImageView) view.findViewById(R.id.contact_photo);
+		mGravatar = (ImageView) view.findViewById(R.id.gravatar);
 		return view;
 	}
 	
 	public void updateContact(long id) {
 		Log.i(TAG, "Update contact detail: " + id);
+		
+		ContentResolver cr = getActivity().getContentResolver();
+		
+		
+		
 		showPhoto(id);
 	}
 	
@@ -49,6 +62,28 @@ public class ContactDetailFragment extends Fragment {
 		
 		if (input != null) 
 			mContactPhoto.setImageBitmap(BitmapFactory.decodeStream(input));
+	}
+	
+	static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
+		Contacts._ID, 
+		Contacts.DISPLAY_NAME, 
+		Contacts.CONTACT_STATUS,
+		Contacts.CONTACT_PRESENCE, 
+		Contacts.PHOTO_ID, 
+		Contacts.LOOKUP_KEY, };
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle arg1) {
+		Uri baseUri = Contacts.CONTENT_URI;
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor arg1) {
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
 	}
 	
 }
