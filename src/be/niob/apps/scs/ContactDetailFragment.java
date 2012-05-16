0abefd438bbf99eeac652a3ee3687fc6038eb021
niob <versluyssander@gmail.com>
2012-05-16 17:40:50 +0200

    load gravatar

--- src/be/niob/apps/scs/ContactDetailFragment.java
+++ src/be/niob/apps/scs/ContactDetailFragment.java
@@ -1,10 +1,13 @@
 package be.niob.apps.scs;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.android.imageloader.ImageLoader;
 
 import android.content.ContentResolver;
 import android.content.ContentUris;
-import android.content.CursorLoader;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -14,38 +17,55 @@ import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class ContactDetailFragment extends Fragment 
 	implements LoaderManager.LoaderCallbacks<Cursor> {
  	
 	private static final String TAG = Util.TAG + ":ContactDetailFragment";
 	
+	private ImageLoader mImageLoader;
+	
 	private ImageView mContactPhoto;
-	private ImageView mGravatar;
+	private LinearLayout mGravatars;
+	
+	private Cursor emailCursor;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View  view = inflater.inflate(R.layout.contact_detail, container);
+		mImageLoader = ImageLoader.get(getActivity());
 		mContactPhoto = (ImageView) view.findViewById(R.id.contact_photo);
-		mGravatar = (ImageView) view.findViewById(R.id.gravatar);
+		mGravatars = (LinearLayout) view.findViewById(R.id.gravatars);
 		return view;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		getLoaderManager().initLoader(0, null, this);
 	}
 	
 	public void updateContact(long id) {
 		Log.i(TAG, "Update contact detail: " + id);
 		
-		ContentResolver cr = getActivity().getContentResolver();
-		
-		
-		
 		showPhoto(id);
+		
+		if (emailCursor != null)
+			emailCursor.close();
+		
+		getLoaderManager().restartLoader((int)id, null, this);
 	}
 	
 	private void showPhoto(long id) {
@@ -64,6 +84,40 @@ public class ContactDetailFragment extends Fragment
 			mContactPhoto.setImageBitmap(BitmapFactory.decodeStream(input));
 	}
 	
+	private void showGravatar() {
+		
+		mGravatars.removeAllViews();
+		
+		if (emailCursor != null) {
+			
+			while (emailCursor.moveToNext()) {
+
+				String email = emailCursor
+						.getString(emailCursor
+								.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+				String emailType = emailCursor
+						.getString(emailCursor
+								.getColumnIndex(ContactsContract.CommonDataKinds.Email.TYPE));
+				
+				LayoutParams params = new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.WRAP_CONTENT, 
+						LinearLayout.LayoutParams.WRAP_CONTENT);
+				
+				ImageView imageView = new ImageView(getActivity());
+				imageView.setLayoutParams(params);
+				imageView.setImageResource(R.drawable.person);
+				mGravatars.addView(imageView);
+				
+				mImageLoader.bind(imageView, Util.Gravatar.url(email), null);		
+				
+				break;
+			}
+
+		} else {
+			Log.d(TAG, "Cursor for email is not loaded");
+		}
+	}
+	
 	static final String[] CONTACTS_SUMMARY_PROJECTION = new String[] {
 		Contacts._ID, 
 		Contacts.DISPLAY_NAME, 
@@ -73,17 +127,23 @@ public class ContactDetailFragment extends Fragment
 		Contacts.LOOKUP_KEY, };
 
 	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle arg1) {
-		Uri baseUri = Contacts.CONTENT_URI;
-		return null;
+	public Loader<Cursor> onCreateLoader(int id, Bundle arg) {
+		Log.d(TAG, "Loader create");
+		Uri uri = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+		String selection = ContactsContract.CommonDataKinds.Phone.CONTACT_ID +"=?";
+		return new CursorLoader(getActivity(), uri, null, selection, new String[] {""+id}, null);
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor arg1) {
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		Log.d(TAG, "Loader finished");
+		emailCursor = cursor;
+		showGravatar();
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
+		Log.d(TAG, "Loader reset");
 	}
 	
 }
--- src/be/niob/apps/scs/Util.java
+++ src/be/niob/apps/scs/Util.java
@@ -1,7 +1,45 @@
 package be.niob.apps.scs;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Util {
 
 	public static final String TAG = "SocialContactSync";
+	
+	public static class Gravatar {
+		
+		private static final int GRAVATAR_DEFAULT_SIZE = 200;
+		
+		public static String hex(byte[] array) {
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i]
+						& 0xFF) | 0x100).substring(1, 3));
+			}
+			return sb.toString();
+		}
+
+		public static String md5Hex(String message) {
+			try {
+				MessageDigest md =
+				MessageDigest.getInstance("MD5");
+				return hex(md.digest(message.getBytes("CP1252")));
+			} catch (NoSuchAlgorithmException e) {
+			} catch (UnsupportedEncodingException e) {
+			}
+			return null;
+		}
+		
+		public static String url(String email) {
+			return url(email, GRAVATAR_DEFAULT_SIZE);
+		}
+		
+		public static String url(String email, int size) {
+			return "http://www.gravatar.com/avatar/" + md5Hex(email) + "?s=" + size + "&d=404";
+		}
+		
+	}
 	
 }
